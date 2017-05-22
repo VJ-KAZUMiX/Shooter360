3a3a52dc204e9e7e9dd3742fb9f71f0cef0895b1
KAZUMiX <kazumix@gmail.com>
2017-05-22 18:56:50 +0900

    prepare TaskSystem

---
+++ Assets/Scripts/KAZUMiX/Task/BaseTaskMonoBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KAZUMiX.Task
+{
+	public class BaseTaskMonoBehaviour : MonoBehaviour, ITask
+	{
+		public ITask prevTask { get; set; }
+
+		public ITask nextTask { get; set; }
+
+		public virtual bool Execute ()
+		{
+			return false;
+		}
+	}
+}
---
+++ Assets/Scripts/KAZUMiX/Task/ITask.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KAZUMiX.Task
+{
+	public interface ITask
+	{
+		/// <summary>
+		/// 前のタスク
+		/// </summary>
+		ITask prevTask {get; set;}
+
+		/// <summary>
+		/// 次のタスク
+		/// </summary>
+		ITask nextTask {get; set;}
+
+		/// <summary>
+		/// 毎フレーム呼び出し
+		/// </summary>
+		bool Execute ();
+	}
+}
